22a80e5cccdbb854b0e63608d21c287102a1be0b
김서연 <2018103585@khu.ac.kr>
2021-05-19 02:28:18 +0900

    의료기관 조회시 목록 출력 성공

--- app.js
+++ app.js
@@ -9,42 +9,46 @@ app.set('views','./views')
 app.set('view engine','pug');
 app.use(express.static('public'));
 
+
 //?page=페이지번호&?perPage=페이지당 데이터수
 const $base_url = `https://api.odcloud.kr/api/apnmOrg/v1/list`;
 const $key = '4US0H%2BXj%2BmS8IR4YL0%2BUb9H4CcbTr92QxhYULfOEz1DT%2BZaaos4sRfNh6cmAD2Icli862Ysc31%2BaE4pWywDBIA%3D%3D';
-
-//임시 매개변수 
 const $page=1;
-const $perPage=10;
+const $perPage=30000;
 
 const $api_url = $base_url + '?serviceKey='+$key+'&page='+$page+'&perPage='+$perPage;
 console.log($api_url);
 
 //해당 URL로 요청 
-request($api_url,function(err,res,body){
-    if(err) throw err;
- 
-    var obj = JSON.parse(body);
-    // console.log(obj);
-    for(var i=0; i<obj.data.length; i++){
-        let name = obj.data[i].orgnm;
-        let location = obj.data[i].orgZipaddr;
-        console.log(`기관명: ${name}, 위치: ${location}\n`);
-    }
+app.post('/',function(req,res,next){
+    const searchWord = req.body.region; //지역 키워드 
 
-    // res.render('main', {title: 'misae', data:data, pm10:pm10})
+    request($api_url,function(err,response,body){
+        if(err) throw err;
+     
+        //data부분만 추출
+        var obj = JSON.parse(body).data;
+
+        // console.log(obj);
+
+        //검색한 지역 포함한 모든 data 담기
+        let searchList = obj.filter(searchList => {
+            return searchList.orgZipaddr.includes(searchWord);
+          });
+        
+        // console.log(searchList);
+        
+        //result라는 변수에 담아 결과 보내기 
+        res.render('main', {result:searchList});
+    })
 
 })
+
 
 app.listen(3000,function(){
     console.log('Connected 3000 port!');
 });
 
-app.post('/search',function(req,res){
-    res.send('내가 검색한 지역은 ' + req.body.region);
-})
-
 app.get('/',function(req,res){
-
     res.render('main');
 })
